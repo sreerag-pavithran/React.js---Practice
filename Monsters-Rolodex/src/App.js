d8def07af9824a26226db341e31d6a4908bf69cd
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-06-23 22:18:29 +0530

    Added Listing

---
+++ Monsters-Rolodex/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList} from './components/card-list/card-list'
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users=> this.setState({ monsters: users }))
+  }
+
+render(){
+  return (
+    <div className="App">
+    <CardList name="Name">
+      <h1>Sreerag</h1>
+    </CardList>
+      {
+        this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1> )
+      }
+    </div>
+  );
+}
+}
+
+export default App;
