150d6aa6fc8542f9be569b9537117fecd26ca821
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-08-05 15:18:27 +0530

    Added BrowserRouter

--- react-ecommerce/src/components/directory/directory.component.jsx
+++ react-ecommerce/src/components/directory/directory.component.jsx
@@ -46,13 +46,11 @@ class Directory extends React.Component {
 
     render(){
         return(
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({ title, id, imageUrl, size, linkUrl }) =>(
-                        <MenuItem key={ id } title={ title } imageUrl= { imageUrl } size={ size } linkUrl={ linkUrl }/>
-                    ))
-                }
-            </div>
+            <div className='directory-menu'>
+              {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                  <MenuItem key={id} {...otherSectionProps} />
+              ))}
+          </div>
         )
     }
 }
--- react-ecommerce/src/index.js
+++ react-ecommerce/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
